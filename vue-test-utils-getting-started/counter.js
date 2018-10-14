50e9be1778efa621ae8c4444fda185429e32c726
Johnny Johnson <johnny2136@gmail.com>
2018-10-13 22:13:10 -0400

    day 81

---
+++ vue-test-utils-getting-started/counter.js
@@ -0,0 +1,22 @@
+// counter.js
+/*jshint esversion: 6 */
+export default {
+  template: `
+    <div>
+      <span class="count">{{ count }}</span>
+      <button @click="increment">Increment</button>
+    </div>
+  `,
+
+  data () {
+    return {
+      count: 0
+    };
+  },
+
+  methods: {
+    increment () {
+      this.count++;
+    }
+  }
+};
