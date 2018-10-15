525f173da6dcea5f3e0c9472d28193607f9f4234
Deven Phillips <InfoSec812@users.noreply.github.com>
2018-10-15 10:59:12 -0400

    Update jest config for working Gherkin Feature tests

--- tdd-vue/jest.config.js
+++ tdd-vue/jest.config.js
@@ -4,15 +4,27 @@ module.exports = {
     "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
     ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
   },
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/**/*.{vue}"],
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: ["json", "lcov", "text", "html"],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
+    }
+  },
   moduleNameMapper: {
     "^vue$": "vue/dist/vue.common.js",
     "src/([^\\.]*).js$": "<rootDir>/src/$1.js",
     "src/([^\\.]*).vue$": "<rootDir>/src/$1.vue",
     "src/([^\\.]*)/([^\\.]*).vue$": "<rootDir>/src/$1/$2.vue",
-    "quasar": "quasar-framework/dist/umd/quasar.mat.umd.min.js"
+    quasar: "quasar-framework/dist/umd/quasar.mat.umd.min.js"
   },
   snapshotSerializers: ["jest-serializer-vue"],
-  // testMatch: ["**/tests/unit/**/*.spec.js"],
   reporters: [
     "default",
     [
